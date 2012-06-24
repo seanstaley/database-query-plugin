d8a246e9fc371486ba4cb48d8da41fdfdc97831d
Sean Staley <sean.staley@jivesoftware.com>
2012-06-23 23:24:43 -0700

    Added more code to activity engine and languages.

---
+++ src/main/java/com/jivesoftware/plugin/dbQuery/dao/ActivityQueryExecute.java
@@ -0,0 +1,10 @@
+package com.jivesoftware.plugin.dbQuery.dao;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Sean M. Staley
+ * Date: 6/23/12
+ * Time: 11:18 PM
+ */
+public class ActivityQueryExecute extends QueryExecute {
+}

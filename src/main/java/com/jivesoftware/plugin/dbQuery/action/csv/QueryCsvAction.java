f359b42d93316d8707d8f50c126a4869c63d3ad8
Ben Walker <ben.walker@jivesoftware.com>
2012-10-16 19:19:05 -0700

    #25: Added Error catching for bad SQL.

--- src/main/java/com/jivesoftware/plugin/dbQuery/action/csv/QueryCsvAction.java
+++ src/main/java/com/jivesoftware/plugin/dbQuery/action/csv/QueryCsvAction.java
@@ -6,6 +6,7 @@ import com.jivesoftware.plugin.dbQuery.dao.query.QueryExecute;
 import com.jivesoftware.util.StringUtils;
 import org.apache.commons.io.IOUtils;
 import org.apache.log4j.Logger;
+import org.springframework.jdbc.BadSqlGrammarException;
 
 import java.io.*;
 import java.util.ArrayList;
@@ -24,6 +25,9 @@ public class QueryCsvAction extends AdminActionSupport {
     private String databaseQuery;
     private boolean selectQuery = true;
 
+
+    private boolean cleanQuery = true;
+
     public String execute() {
         ArrayList<ArrayList<String>> arrayOfRows;
 
@@ -32,16 +36,26 @@ public class QueryCsvAction extends AdminActionSupport {
         }
 
         //Is the query NOT a SELECT query?
-        else if (!queryExecute.validateSelectQuery(databaseQuery)) {
+        if (!queryExecute.validateSelectQuery(databaseQuery)) {
             setSelectQuery(false);
             return INPUT;
         }
 
-        arrayOfRows = queryExecute.returnQueryResults(databaseQuery);
-        response.setHeader("Cache-Control", "private");
-        String csv = generateCsv(arrayOfRows);
-        InputStream stream = createCsvStream(csv);
-        setCsvStream(stream);
+        try {
+            arrayOfRows = queryExecute.returnQueryResults(databaseQuery);
+            response.setHeader("Cache-Control", "private");
+            String csv = generateCsv(arrayOfRows);
+            InputStream stream = createCsvStream(csv);
+            setCsvStream(stream);
+
+        }
+        catch (BadSqlGrammarException bse) {
+            log.error("Database Query Plugin: Bad SQL grammar when querying Application Database by " +
+                    getUser().getUsername());
+            setCleanQuery(false);
+            return INPUT;
+
+        }
         return "export";
     }
 
@@ -156,4 +170,13 @@ public class QueryCsvAction extends AdminActionSupport {
     public void setSelectQuery(boolean selectQuery) {
         this.selectQuery = selectQuery;
     }
+
+    public boolean isCleanQuery() {
+        return cleanQuery;
+    }
+
+    public void setCleanQuery(boolean cleanQuery) {
+        this.cleanQuery = cleanQuery;
+    }
+
 }
